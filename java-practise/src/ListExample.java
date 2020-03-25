bd64b1ea81423b9c973b6d7ae81da65c474a95b3
agopalaiah <agopalaiah@quotient.com>
2020-03-25 19:43:48 +0530

    dsc

---
+++ java-practise/src/ListExample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <String> A = new ArrayList <String> ();
+//		A.add(9);
+		System.out.println(A);
+		A.add("a");
+		System.out.println(A);
+		System.out.println(A.get(0) instanceof String);
+		
+
+	}
+
+}
