a1aa65064b511ed636369a37ac71094063493ef9
agopalaiah <agopalaiah@quotient.com>
2020-03-26 13:51:42 +0530

    dijkstra algo, stdin

---
+++ java-practise/src/eclipseShortCut/App.java
@@ -0,0 +1,17 @@
+package eclipseShortCut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+	public static void main(String[] args) {
+		
+		List list;
+		ArrayList another;
+		another = new ArrayList();
+		
+		
+		
+
+	}
+}
